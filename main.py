afa08c15260579b26ef22bd7cda4c289cc9420ef
Jonathan Hirsch <112533132+Jon-H03@users.noreply.github.com>
2023-01-26 13:38:34 -0800

    Update main.py

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ header = {
 
 }
 
-GOOGLE_SHEETS_URL = "https://docs.google.com/forms/d/e/1FAIpQLSeRlP-L8yrF2aEGMwUnin230wd8tf1ELpOW2UFogRCGQPwSFA/viewform?usp=pp_url"
-ZILLOW_URL = "https://www.zillow.com/homes/for_rent/?searchQueryState=%7B%22usersSearchTerm%22%3A%22Portland%2C%20OR%22%2C%22mapBounds%22%3A%7B%22west%22%3A-123.00571444677735%2C%22east%22%3A-122.38567355322266%2C%22south%22%3A45.2777852256197%2C%22north%22%3A45.831669610234385%7D%2C%22isMapVisible%22%3Afalse%2C%22filterState%22%3A%7B%22price%22%3A%7B%22min%22%3A310199%2C%22max%22%3A387749%7D%2C%22mp%22%3A%7B%22min%22%3A1600%2C%22max%22%3A2000%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22ah%22%3A%7B%22value%22%3Atrue%7D%2C%22lau%22%3A%7B%22value%22%3Atrue%7D%2C%22parka%22%3A%7B%22value%22%3Atrue%7D%7D%2C%22isListVisible%22%3Atrue%2C%22customRegionId%22%3A%226cdcc634edX1-CRsgdw3179mg72_13hj4h%22%2C%22pagination%22%3A%7B%7D%2C%22mapZoom%22%3A11%7D"
+GOOGLE_SHEETS_URL = my_google_sheets_url
+ZILLOW_URL = my_zillow_preferences_url
 
 
 response = requests.get(ZILLOW_URL, headers=header)
